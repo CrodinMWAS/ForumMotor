719fa6b77135d2943bf1a0e2d88070025c56ef46
CrodinMWAS <MWASiskola@gmail.com>
2024-11-21 13:37:20 +0100

    init

---
+++ Models/ForumUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumMotor.Models
+{
+    public class ForumUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LinganGuli {  get; set; }
+        public int Guli {  get; set; }
+    }
+}
